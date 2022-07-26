0f03f382f7889c1d6ef9f9d02256589280123293
sanjay7900 <singhsanjay790043@gmail.com>
2022-07-26 12:17:58 +0530

    initial commit

---
+++ EmployeeAssignment_26_07_WebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeAssignment_26_07_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAssignment_26_07_WebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private WebAppDbContext _context;
+        public EmployeeController(WebAppDbContext webAppDbContext)
+        {
+            _context = webAppDbContext; 
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Data(Employee employee)
+        {
+            try
+            {
+                _context.Employee.Add(employee);
+                _context.SaveChanges();
+                return Content($"Employee Registered Successfully  with {employee.FirstName}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);  
+            }
+
+            return Content($"OOps, SomeThing Wrong");
+        }
+    }
+}
---
+++ EmployeeAssignment_26_07_WebApp/WebAppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeAssignment_26_07_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAssignment_26_07_WebApp
+{
+    public class WebAppDbContext:DbContext
+    {
+        public DbSet<Employee> Employee { set; get; }
+        public  WebAppDbContext() { }
+        public WebAppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder option)
+        {
+            option.UseSqlServer(@"Data Source=DESKTOP-AMR2CQS\MSSQLSERVER01;Initial Catalog=EmployeeAssignmentWebApp;Integrated Security=True");
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Employee>().HasIndex(Emp => Emp.Email).IsUnique();
+            
+
+        }
+
+    }
+}
